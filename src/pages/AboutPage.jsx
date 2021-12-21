19aa283da39e9a5f525ebe19e1dd1d83508aba5c
Rush Morgan <51529432+Ounza@users.noreply.github.com>
2021-12-21 15:05:46 +0300

    Add files via upload

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+import Card from "../components/shared/Card"
+function AboutPage() {
+    return (
+        <Card>
+        <div className="about">
+            <h1>About this Project</h1>
+            <p>This is a React App to leave feedback for a product/service</p>
+            <p>Version: 1.0.0</p>
+            <p>
+                <Link to='/'>Back To Home</Link>
+            </p>
+        </div>
+        </Card>
+    )
+}
+
+export default AboutPage
